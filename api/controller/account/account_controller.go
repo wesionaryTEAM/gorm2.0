1cb4cd62c0a48083c95b4447d9619f6da3f42e29
Suz'l Shrestha <sujal.kokh@gmail.com>
2020-07-15 16:48:58 +0545

    accouont controller completed

---
+++ api/controller/account/account_controller.go
@@ -0,0 +1,66 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm2.0/model"
+)
+
+type AccountController interface {
+	GetAccounts(c *gin.Context)
+	AddAccount(c *gin.Context)
+	DeleteAccount(c *gin.Context)
+}
+
+type accountController struct {
+	accountService model.AccountService
+}
+
+//Constructor Function
+func NewAccountController(service model.AccountService) AccountController {
+	return &accountController{
+		accountService: service,
+	}
+}
+
+//GET
+func (a *accountController) GetAccounts(c *gin.Context) {
+	accounts, err := a.accountService.FindAll()
+	if err !== nil {
+		C.JSON(http.StatusBadRequest, gin.H{"error": "Error while fetching the accounts"})
+		return
+	}
+
+	c.JSON(http.StatusOK, accounts)
+}
+
+//POST
+func (a *accountController) AddAccount(c *gin.Context) {
+	var account model.Account
+	if err := c.ShouldBindJSON(&account); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	a.accountService.Create(&account)
+
+	c.JSON(http.StatusOK, account)
+}
+
+//DELETE
+func (a *accountController) DeleteAccount(c *gin.Context) {
+	var account model.Account
+	if err := c.ShouldBindJSON(&account); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	} 
+
+	ErrOnDelete := a.accountService.Delete(&account)
+	if ErrOnDelete!= nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Could not delete the account"})
+		return
+	}
+
+	c.JSON(http.StatusOK, account)
+}
